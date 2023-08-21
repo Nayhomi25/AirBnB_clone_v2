7a1c6deefb1b0d7fa987c60e9f5a3b9fc91525a6
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-08-21 16:46:40 -0700

    added test cases for user.py, review.py and file_storage.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" A test file"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
@@ -8,22 +8,28 @@ class test_review(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ testing review __init__"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ testing review place_id attr"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(type(new.place_id), str if
+                         os.getenve('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(new.user_id), str if 
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
