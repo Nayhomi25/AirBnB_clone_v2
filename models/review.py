3d54bdea6458d1513b9a97526ad06bcbd05c1018
EllaRepo <eliaberango@gmail.com>
2023-08-22 09:30:45 -0400

    Fix erros

--- models/review.py
+++ models/review.py
@@ -3,17 +3,17 @@
 from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from os import getenv
 
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    if storage_type == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         text = Column(String(128), nullable=False)
-        place_id = Column(String(128), ForeignKey(places.id), nullable=False)
-        user.id = Column(String(128), ForeignKey(users.id), nullable=False)
+        place_id = Column(String(128), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(128), ForeignKey('users.id'), nullable=False)
     else:
         place_id = ""
         user_id = ""
