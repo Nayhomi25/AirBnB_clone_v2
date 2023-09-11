6464d6b7edf1b3d3e154725d31128d1a870923ec
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-09-10 18:10:58 -0700

    Revert "successfully deployed"
    This reverts commit 532edd95dc3f2d4179ee417dd8c2476b4a1aebf7.
    
    mistake

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,77 +4,36 @@ Script that distributes an archive to your web servers,
 using the function do_deploy.
 """
 from fabric.api import *
-import os.path
+from os.path import exists, join
 from datetime import datetime
 
 env.hosts = ["18.233.62.178", "174.129.55.177"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/id_rsa"
-
-run_locally = True
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    Distributes an archive to the web servers.
+    """
+    if not exists(archive_path):
         return False
 
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    filename = archive_path.split('/')[-1]
+    folder_name = filename.split('.')[0]
+    no_tgz = join("/data/web_static/releases", folder_name)
+    tmp = join("/tmp", filename)
 
-    local("cp {} /tmp/{}".format(archive_path, file))
-    local(
-        "rm -rf /data/web_static/releases/{}/"
-        .format(name))
-    local(
-        "mkdir -p /data/web_static/releases/{}/"
-        .format(name))
-    local(
-        "tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(file, name))
-    local("rm /tmp/{}".format(file))
-    local(
-        "mv /data/web_static/releases/{}/web_static/*"
-        " /data/web_static/releases/{}/"
-        .format(name, name))
-    local(
-        "rm -rf /data/web_static/releases/{}/web_static"
-        .format(name))
-    local("rm -rf /data/web_static/current")
-    local(
-        "ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(name))
+    try:
+        put(archive_path, "/tmp/")
+        release_path = join("/data/web_static/releases", folder_name)
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf {} -C {}".format(tmp, release_path))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}".format(release_path, release_path))
+        run("rm -rf {}/web_static".format(release_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_path))
+        print("New version deployed!")
+        return True
+    except Exception as error:
         return False
-    if run("rm -rf /data/web_static/releases/{}/"
-           .format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/"
-           .format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-           .format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static"
-           .format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-           .format(name)).failed is True:
-        return False
-
-    print("New version deployed!")
-    return True
