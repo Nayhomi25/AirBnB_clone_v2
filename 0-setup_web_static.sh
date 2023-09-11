5d77ac9db0ba7ffc01f4fafae7bc50acb2ab8aba
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-09-10 17:15:27 -0700

    Setting up servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,43 @@
-#!/usr/bin/env bash
-# Bash Script to Setup Airbnb Static_web 
-sudo apt-get update -y
-sudo apt-get updgrade -y
-sudo apt-get install -y nginx
-mkdir -p  /data/web_static/
-mkdir -p /data/web_static/releases/test/
-mkdir /data/web_static/shared/
-echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current 
-chown ubuntu:ubuntu -hR /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart 
+#!/bin/usr/env bash
+# Sets up the web servers for the deployement of web_static
+
+apt-get update
+apt-get -y install nginx
+
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Hello AirBnB, Ready to be deployed?
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -hR ubuntu:ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-enabled/default
+
+service nginx restart
